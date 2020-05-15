33e8ed42d187329d80f225aaf205d211b732c72d
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-14 18:22:17 -0600

    Added DbSet

--- AspNetCoreTodo/Data/ApplicationDbContext.cs
+++ AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -6,11 +6,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreTodo.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<TodoItem> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }

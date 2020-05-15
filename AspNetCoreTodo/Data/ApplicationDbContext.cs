95a70085a8ba4927d67dfa0bb0fe891d64978701
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-15 10:53:15 -0600

    Added to DbContext

--- AspNetCoreTodo/Data/ApplicationDbContext.cs
+++ AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace AspNetCoreTodo.Data
-{
-    public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
-    }
-}
+﻿using System;using System.Collections.Generic;using System.Text;using AspNetCoreTodo.Models;using Microsoft.AspNetCore.Identity.EntityFrameworkCore;using Microsoft.EntityFrameworkCore;namespace AspNetCoreTodo.Data{    public class ApplicationDbContext : IdentityDbContext    {        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)        {        }        public DbSet<TodoItem> Items { get; set; }        protected override void OnModelCreating(ModelBuilder builder)        {            base.OnModelCreating(builder);            // ...        }    }}
